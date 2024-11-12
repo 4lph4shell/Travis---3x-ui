fb05d4fb6ba63f12311decb8105999d78ad48ae4
4lph4shell <4lph4ir@gmail.com>
2024-11-12 19:59:50 +0330

    add file

---
+++ lang/en.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    'title_serch_page' => 'Search Results | STRAWBERRYVPN™',
+    'title_index_page' => 'Service Information | STRAWBERRYVPN™',
+    'search_bar' => 'Enter your username',
+    'button_search' => 'Search',
+    'config_name' => 'Config Name',
+    'gb' => 'Gigabytes',
+    'day' => 'Day',
+    'search_status' => 'Your Service Details| STRAWBERRYVPN™',
+    'config_status' => 'Config Status',
+    'upload' => 'Upload',
+    'download' => 'Download',
+    'total' => 'Total Size',
+    'linkdownload' => 'Download Applications',
+    'baghimande' => 'Remaining Size',
+    'total_traffic' => 'Consumption Amount',
+    'expiry_date' => 'Expiration Date',
+    'remaining_days' => 'Days Left',
+    'select_lang' => '🌐Select language',
+    'server_name_english' => ' Server Location English ',
+    'server_name_germany' => '  Server Location Germany ',
+    'go_home' => 'Back to Home',
+    'server_name_tehran' => ' Server Location Tehran '
+
+];
