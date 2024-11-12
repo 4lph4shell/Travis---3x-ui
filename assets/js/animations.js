645002c76fe6d076b1e0c8cead09ac7ed52bbad6
4lph4shell <4lph4ir@gmail.com>
2024-11-12 20:07:22 +0330

    edit to add copywrite

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,3 +1,11 @@
+/*<!-- <meta name="subject" content="Travis">
+<meta name="copyright" content="4lph4-travis">
+<meta name="author" content="A747iR, 4lph4-ir@gmail.com">
+<meta name="designer" content="A747ir">
+<meta name="owner" content="4lph4Ir">
+*/
+
+
 $(document).ready(function () {
 
     $('.who-we-are-section .counter').each(function () {
@@ -11,7 +19,6 @@ $(document).ready(function () {
             }
         });
     });
-    
-    });
-    
- 
+
+});
+
--- functions.php
+++ functions.php
@@ -1,5 +1,15 @@
-<?php
+  <!-- <meta name="subject" content="Travis">
+       <meta name="copyright" content="4lph4-travis">
+       <meta name="author" content="A747iR, 4lph4-ir@gmail.com">
+       <meta name="designer" content="A747ir">
+       <meta name="owner" content="4lph4Ir">
+-->
 
-function url_encode_full ($value) {
-return implode("", array_map(function($i) { return sprintf("%%%X", ord($i)); }, str_split($value)));
-}
+  <?php
+
+    function url_encode_full($value)
+    {
+        return implode("", array_map(function ($i) {
+            return sprintf("%%%X", ord($i));
+        }, str_split($value)));
+    }
